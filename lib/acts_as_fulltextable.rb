c6a6c70f507b6c1a9d0d65f6ad830f2dce90a797
Henrique Sobral Gomes <henrique@devideia.com.br>
2014-05-20 22:20:23 -0300

    rails 4 ajustes

--- lib/acts_as_fulltextable.rb
+++ lib/acts_as_fulltextable.rb
@@ -86,7 +86,7 @@ module ActsAsFulltextable
             return
           end
         end
-        FulltextRow.update_all(["value = ?, parent_id = ?", self.fulltext_value, self.parent_id_value], ["fulltextable_type = ? AND fulltextable_id = ?", self.class.to_s, self.id]) if !(self.class.fulltext_options[:check_for_changes]) || (row.value != self.fulltext_value) || (self.parent_id_value != row.parent_id)
+        FulltextRow.where(["fulltextable_type = ? AND fulltextable_id = ?", self.class.to_s, self.id]).update_all(["value = ?, parent_id = ?", self.fulltext_value, self.parent_id_value]) if !(self.class.fulltext_options[:check_for_changes]) || (row.value != self.fulltext_value) || (self.parent_id_value != row.parent_id)
       else
 
         row = FulltextRow.find_by(fulltextable_type: self.class.to_s, fulltextable_id: self.id)
